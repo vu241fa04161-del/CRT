615f827f7c59ea3b732c8e6331178da54a53d545
vu241fa04161-del <vu.241fa04161>
2026-06-02 10:58:29 +0530

    final code

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
